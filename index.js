3a5b1abab13f9a094a3b21d86eb8a0d98a7b2aec
Muhammad Hussain AbdulRehman <p.rehman2005@gmail.com>
2026-08-17 15:58:23 +0500

    juhdfsd

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express'
 const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello! Welcome to my Express server 2👋')
+})
+
 app.listen(8001, () => {
   console.log('App running on port 8001')
 })
